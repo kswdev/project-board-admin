438131e1f757f0dbe78c48ecbd135e5684d185fa
jogony <seonwongim@gmail.com>
2023-03-26 16:37:37 +0900

    #2 - 도메인 설계: 회원 엔티티, 권한 enum, converter(Set -> ", , ,") 회원 체계는 반드시 들어갈 것으로 예상하므로 도메인 설계를 함
    권한 체계가 새로 들어갔고, 권한을 db 컬럼에 작성할 때
    하나의 회원에 여러 권한을 쥑 위해
    권한 테이블을 새로 만드는 방법이 부담스럼다.
    
    권한 정보를 합쳐진 문자열로 저장하게끔 하고,
    이를 컨버터로 구현함
    
    새로운 권한 체계는 'RoleType' enum으로 소개함
    
    스프링 시큐리티 인증 데이터의 작성
    principal, 카카오 인증 api의 응답, 그리고 테스트 작성

---
+++ src/main/java/com/boardadmin/project/dto/security/BoardAdminPrincipal.java
@@ -0,0 +1,89 @@
+package com.boardadmin.project.dto.security;
+
+
+import com.boardadmin.project.domain.constant.RoleType;
+import com.boardadmin.project.dto.UserAccountDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record BoardAdminPrincipal(
+        String username,
+        String password,
+        Collection<? extends GrantedAuthority> authorities,
+        String email,
+        String nickname,
+        String memo,
+        Map<String, Object> oAuth2Attribute
+) implements UserDetails, OAuth2User {
+
+    public static BoardAdminPrincipal of(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo) {
+        return of(username, password, roleTypes, email, nickname, memo, Map.of());
+    }
+
+    public static BoardAdminPrincipal of(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo, Map<String, Object> oAuth2Attribute) {
+        return new BoardAdminPrincipal(
+                username,
+                password,
+                roleTypes.stream()
+                        .map(RoleType::getRoleName)
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toUnmodifiableSet()),
+                email,
+                nickname,
+                memo,
+                oAuth2Attribute
+        );
+    }
+
+    public static BoardAdminPrincipal from(UserAccountDto dto) {
+        return BoardAdminPrincipal.of(
+                dto.userId(),
+                dto.userPassword(),
+                dto.roleTypes(),
+                dto.email(),
+                dto.nickname(),
+                dto.memo()
+        );
+    }
+
+    public UserAccountDto toDto() {
+        return UserAccountDto.of(
+                username,
+                password,
+                authorities.stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .map(RoleType::valueOf)
+                        .collect(Collectors.toUnmodifiableSet())
+                ,
+                email,
+                nickname,
+                memo
+        );
+    }
+
+    @Override public String getPassword() { return this.password; }
+    @Override public String getUsername() { return this.username; }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+
+    @Override
+    public Map<String, Object> getAttributes() { return oAuth2Attribute; }
+
+    @Override
+    public String getName() { return username; }
+
+}
