e41289c61de01ac77a27002e4b017649b50fc93b
jogony <seonwongim@gmail.com>
2023-04-14 01:37:21 +0900

    #15 - 회원 관리 페이지의 컨트롤러, 서비스, 뷰 구현

--- src/main/java/com/boardadmin/project/controller/UserAccountManagementController.java
+++ src/main/java/com/boardadmin/project/controller/UserAccountManagementController.java
@@ -1,18 +1,40 @@
 package com.boardadmin.project.controller;
 
+import com.boardadmin.project.dto.response.UserAccountResponse;
+import com.boardadmin.project.service.UserAccountManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
 @Controller
 public class UserAccountManagementController {
 
+    private final UserAccountManagementService userAccountManagementService;
+
     @GetMapping
-    public String userAccounts() {
+    public String userAccounts(Model map) {
+        map.addAttribute("userAccounts",
+                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
+        );
+
         return "management/user-accounts";
+    }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserAccountResponse userAccount(@PathVariable String userId) {
+        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+    }
+
+    @PostMapping("/{userId}")
+    public String deleteUserAccount(@PathVariable String userId) {
+        userAccountManagementService.deleteUserAccount(userId);
+
+        return "redirect:/management/user-accounts";
     }
 
 }
