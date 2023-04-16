2c8a5fc5ef36b4f4e17064b339c44e1eff2fe19a
jogony <seonwongim@gmail.com>
2023-04-16 19:15:24 +0900

    뷰 - 간단하게 웹 소켓 이용한 채팅 기능 추가

---
+++ src/main/java/com/boardadmin/project/controller/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.boardadmin.project.controller;
+
+import com.boardadmin.project.dto.websocket.WebSocketMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/chat")
+    public WebSocketMessage chat(WebSocketMessage message, Principal principal) {
+        return WebSocketMessage.of("안녕하세요 " + principal.getName() + "! " + message.content() + "라고 하셨나요?");
+    }
+}
---
+++ src/main/java/com/boardadmin/project/dto/websocket/WebSocketMessage.java
@@ -0,0 +1,7 @@
+package com.boardadmin.project.dto.websocket;
+
+public record WebSocketMessage(String content) {
+    public static WebSocketMessage of (String content) {
+        return new WebSocketMessage(content);
+    }
+}
